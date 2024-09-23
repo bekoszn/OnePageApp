36cb6356944d64cd013cb1b57b1b911a95dc59c2
bekoszn <berkeozguder@gmail.com>
2024-09-23 16:10:36 +0300

    Initial Commit

---
+++ OnePageAppTests/OnePageAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnePageAppTests.swift
+//  OnePageAppTests
+//
+//  Created by Berke Özgüder on 23.09.2024.
+//
+
+import Testing
+@testable import OnePageApp
+
+struct OnePageAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
